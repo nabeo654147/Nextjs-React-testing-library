fe2ae7d2e7fb4c724733e0686f0a855af5795c6d
nabeo654147 <nabehiko616@gmail.com>
2022-01-25 19:30:45 +0900

    post&blog-pageのデータ取得

--- src/pages/blog-page.tsx
+++ src/pages/blog-page.tsx
@@ -1,10 +1,28 @@
 import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/fetch";
+import Post from "../components/Post";
+import { GetStaticProps } from "next";
+import { POST } from "../../types/Types";
 
-const BlogPage: React.FC = () => {
+type STATICPROPS = {
+  posts: POST[];
+};
+
+const BlogPage: React.FC<STATICPROPS> = ({ posts }) => {
   return (
     <Layout title="Blog">
       <p className="text-4xl">blog page</p>
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} {...post} />)}
+      </ul>
     </Layout>
   );
 };
 export default BlogPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+};
